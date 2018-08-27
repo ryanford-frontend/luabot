c5450696ceb891532e83ebb0aadba54fee1d7858
Ryan Ford <ryanford@email.com>
2018-08-27 12:31:02 +0800

    get room resource instead of just id

--- spec/get_room_spec.lua
+++ spec/get_room_spec.lua
@@ -1,33 +1,33 @@
-local get_id = require('get_id')
+local get_room = require('get_room')
 
-describe('get_id', function()
-   local ROOM = 'gitterhq/sandbox'
+describe('get_room', function()
+   local URI = 'gitterhq/sandbox'
    local GITTER_TOKEN = os.getenv('GITTER_TOKEN')
 
    it('returns a table when required', function()
-      local module_type = type(get_id)
+      local module_type = type(get_room)
       local expected = 'table'
       assert.are.equal(module_type, expected)
    end)
 
-   it('returns an id as a string when called', function()
-      local id = get_id(ROOM, GITTER_TOKEN)
-      local id_type = type(id)
+   it('returns an room as a string when called', function()
+      local room = get_room(URI, GITTER_TOKEN)
+      local room_type = type(room)
       local expected = 'string'
-      assert.are.equal(id_type, expected)
+      assert.are.equal(room_type, expected)
    end)
 
    it('returns nil and error if no room uri', function()
-      local id, err = get_id(nil, GITTER_TOKEN)
+      local room, err = get_room(nil, GITTER_TOKEN)
       local expected = 'No room uri given'
-      assert.has_no(id)
+      assert.has_no(room)
       assert.are.equal(err, expected)
    end)
 
    it('returns nil and error if no token', function()
-      local id, err = get_id('gitterhq/sandbox')
+      local room, err = get_room('gitterhq/sandbox')
       local expected = 'No token given'
-      assert.has_no(id)
+      assert.has_no(room)
       assert.are.equal(err, expected)
    end)
 end)
